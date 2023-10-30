79163bee2108d46cc960c41c9d7c6c6672dc7f2d
pgmpablo157321 <pablo.gonzalez@factored.ai>
2023-10-30 15:55:53 -0500

    Stable Diffusion Reference Implementation

---
+++ text_to_image/tools/latent.py
@@ -0,0 +1,53 @@
+import torch
+from diffusers import EulerDiscreteScheduler
+from diffusers.utils.torch_utils import randn_tensor
+
+
+def create_latents(
+    batch_size,
+    num_channels_latents,
+    height,
+    width,
+    dtype,
+    generator,
+    vae_scale_factor,
+    scheduler,
+):
+    shape = (
+        batch_size,
+        num_channels_latents,
+        height // vae_scale_factor,
+        width // vae_scale_factor,
+    )
+    latents = randn_tensor(shape, generator=generator, dtype=dtype)
+
+    # scale the initial noise by the standard deviation required by the scheduler
+    latents = latents * scheduler.init_noise_sigma
+    return latents
+
+
+if __name__ == "__main__":
+    batch_size = 1
+    num_channels_latents = 4
+    height = 1024
+    width = 1024
+    dtype = torch.float32
+    seed = 0
+    generator = torch.Generator().manual_seed(seed)
+    vae_scale_factor = 8
+    scheduler = EulerDiscreteScheduler.from_pretrained(
+        "stabilityai/stable-diffusion-xl-base-1.0", subfolder="scheduler"
+    )
+    save_path = "latents.pt"
+
+    latents = create_latents(
+        batch_size,
+        num_channels_latents,
+        height,
+        width,
+        dtype,
+        generator,
+        vae_scale_factor,
+        scheduler,
+    )
+    torch.save(latents, save_path)
